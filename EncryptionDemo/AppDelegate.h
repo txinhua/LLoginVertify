b4787e840b9ddb23416644f5219173f038701823
gftang <gftang@vcainfo.com>
2016-07-07 16:39:01 +0800

    version 1.0

---
+++ EncryptionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EncryptionDemo
+//
+//  Created by VcaiTech on 16/7/7.
+//  Copyright © 2016年 VcaiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EncryptionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EncryptionDemo
+//
+//  Created by VcaiTech on 16/7/7.
+//  Copyright © 2016年 VcaiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
